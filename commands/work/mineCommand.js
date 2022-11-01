8bc6153b958b343718eae7e9583bc1ceca8919af
Bread <72562619+NotRealArif@users.noreply.github.com>
2022-11-01 11:55:52 +0000

    Some bug fixes

---
+++ commands/work/mineCommand.js
@@ -0,0 +1,12 @@
+import { User } from "../../database/profile.js";
+import config from "../../config.json" assert { type: "json" };
+
+export default {
+  data: {
+    name: "mine",
+    description: "Let's mine something today!"
+  },
+  run: async (client, interaction) => {
+    
+  }
+}
