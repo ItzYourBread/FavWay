cd038fd06e3255c94aefac2c9dde2f69c80c1f26
Bread <72562619+NotRealArif@users.noreply.github.com>
2022-11-01 10:17:36 +0000

    Ready.js is fixed

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,8 @@ import { colour } from "printly.js";
 
 export function ready(client) {
   client.on("ready", async () => {
+    client.editStatus("idle", { name: "FavWay", type: 0 });
+    console.log(colour.greenBright(`[Discord API] ${client.user.username} is now connected to Discord!`));
   });
   console.log(colour.cyanBright("[Event] ready.js is loaded"));
 }
