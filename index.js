645c6df612405e761c4218e307720d9df33feba4
Bread <72562619+NotRealArif@users.noreply.github.com>
2022-11-03 11:31:37 +0000

    Everything moved to src folder and working fine!!

---
+++ index.js
@@ -0,0 +1,1 @@
+import "./src/favway.js";
--- src/handlers/commands.js
+++ src/handlers/commands.js
@@ -1,22 +1,24 @@
 import { readdirSync } from "fs";
+import path from "path";
 import { colour } from "printly.js";
 
 const commands = [];
 export { commands }
 
+const commandsPath = path.join('./src/commands');
+const commandsImport = path.join('..', 'commands');
+
 export function loadCommands(client) {
   client.on("ready", async () => {
-    const commandFolders = readdirSync("./commands");
+    const commandFolders = readdirSync(`${commandsPath}`);
     for (const folder of commandFolders) {
-      const commandFiles = readdirSync(`./commands/${folder}`)
+      const commandFiles = readdirSync(`${commandsPath}/${folder}`)
         .filter((file) => file.endsWith(".js"));
       for (const file of commandFiles) {
-        const slashCommandObject = await import(`../commands/${folder}/${file}`);
-
+        const slashCommandObject = await import(`${commandsImport}/${folder}/${file}`);
         if (slashCommandObject.default.data.name) {
           console.log(colour.cyanBright(`[Command] ${slashCommandObject.default.data.name} is loaded`));
         }
-
         // await client.createGuildCommand(process.env.SLASH_COMMANDS_GUILD, slashCommandObject.default.data)
         // Use code underneath for global slash commands
         await client.createCommand(slashCommandObject.default.data)
