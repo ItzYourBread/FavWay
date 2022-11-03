af43cc5a42187870adbc530d6eccc1da8176a82a
Bread <72562619+NotRealArif@users.noreply.github.com>
2022-11-03 18:15:57 +0000

    Add status webhook

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,10 +1,36 @@
 import Eris from 'eris';
 import { colour } from "printly.js";
+import config from "../config.json" assert { type: "json" };
 
-export function ready(client) {
+let statusMessageID = "1037785678542557264";
+
+export function ready(client, message) {
   client.on("ready", async () => {
-    client.editStatus("idle", { name: "FavWay", type: 0 });
+    client.editStatus("online", { name: "FavWay", type: 0 });
     console.log(colour.greenBright(`[Discord API] ${client.user.username} is now connected to Discord!`));
+
+    if (config.webhooks.status.mode) {
+      setInterval(() => {
+        let status = {
+          color: Number(config.colours.embed),
+          fields: [
+            {
+              name: "Status",
+              value: `**FavWay** is now online!`,
+              inline: true
+            }
+          ],
+          timestamp: new Date()
+        }
+          client.editWebhookMessage(
+            config.webhooks.status.id, 
+            config.webhooks.status.token, 
+            statusMessageID, 
+            { embeds: [status] }
+          );
+        console.log("refreshing");
+      }, 10000);
+    }
   });
   console.log(colour.cyanBright("[Event] ready.js is loaded"));
 }
