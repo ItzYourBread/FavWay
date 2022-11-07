9d132dfd68073eb5154b0a6981dd69f9f357d643
Bread <72562619+NotRealArif@users.noreply.github.com>
2022-11-07 21:14:03 +0000

    Updated slots command

--- src/commands/gamble/slotsCommand.js
+++ src/commands/gamble/slotsCommand.js
@@ -1,6 +1,10 @@
 import { User } from "../../database/profile.js";
 import config from "../../config.json" assert { type: "json" };
 import { Constants } from "eris";
+import { setTimeout as wait } from "node:timers/promises";
+
+const slots = ["<:emoji_20:1039281159345029200>", "<:emoji_20:1039281175950278786>"];
+const loading = "<a:emoji_19:1039281105955721227><a:emoji_19:1039281105955721227><a:emoji_19:1039281105955721227>";
 
 export default {
   data: {
@@ -8,7 +12,7 @@ export default {
     description: "Let's play with slots machine today!",
     options: [{
       name: "bet",
-      description: "Please enter your bet, 500 to 250,000",
+      description: "Please enter your bet, 200 to 20,000",
       type: Constants.ApplicationCommandOptionTypes.INTEGER,
       required: true
     }],
@@ -18,9 +22,64 @@ export default {
     const user = interaction.member;
     const userData = await User.findOne({ id: user.id }) || new User({ id: user.id });
 
-    let test = {
-      description: `${slots} ${moving}`
+    const bet = interaction.data.options[0].value;
+
+    if (bet < 200 || bet > 20000 ) {
+      return interaction.createMessage({
+        content: `You only can bet 200 to 20,000!`,
+        flags: 64
+      });
     }
-    await interaction.createMessage({ embeds: [test] });
+
+    if (userData.coins < bet) {
+      return interaction.createMessage({
+        content: `You don't have ${bet.toLocaleString()} in your balance, You only have ${userData.coins.toLocaleString()}.`,
+        flags: 64
+      });
+    }
+
+    await interaction.createMessage({
+      embeds: [{
+        title: `Slots`,
+        color: Number(config.colours.embed),
+        description: `|⎯⎯⎯⎯|\n|${loading} |\n|⎯⎯⎯⎯|`,
+        timestamp: new Date()
+      }],
+    });
+    
+    let number = [];
+    let amount = "";
+    let win;
+    for (let i = 0; i < 3; i++) { number[i] = Math.floor(Math.random() * slots.length); }
+
+    if (number[0] == number[1] && number[1] == number[2])  {
+        amount = bet * 0.9;
+        win = true;
+    } else if (number[0] == number[1] || number[0] == number[2] || number[1] == number[2]) { 
+        amount = bet * 0.5;
+        win = true;
+    }
+    
+    if (win) {
+      await wait(3500);
+      await interaction.editOriginalMessage({
+        embeds: [{
+          title: `Slots`,
+          color: Number(config.colours.success),
+          description: `|⎯⎯⎯⎯|\n|${slots[number[0]]}${slots[number[1]]}${slots[number[2]]} |\n|⎯⎯⎯⎯|\nYou won **${config.emojis.coin}${amount.toLocaleString()}** and now you have **${config.emojis.coin}${userData.coins}**`,
+          timetamp: new Date()
+        }],
+      });
+    } else {
+      await wait(3400);
+      await interaction.editOriginalMessage({
+        embeds: [{
+          title: `Slots`,
+          color: Number(config.colours.error),
+          description: `|⎯⎯⎯⎯|\n|${slots[number[0]]}${slots[number[1]]}${slots[number[2]]} |\n|⎯⎯⎯⎯|\nSad to say but you lost **${config.emojis.coin}${amount.toLocaleString()}** :(`,
+          timeStamp: new Date()
+        }],
+      });
+    }
   }
 }
--- src/commands/statistics/balanceCommand.js
+++ src/commands/statistics/balanceCommand.js
@@ -13,7 +13,7 @@ export default {
     
     let balance = {
       title: `${user.username}'s Balance`,
-      color: 0xcec6ff,
+      color: Number(config.colours.embed),
       description: `**Coin:** ${config.emojis.coin}${userData.coins.toLocaleString()}\n**Gem:** ${config.emojis.gem}${userData.gems.toLocaleString()}`,
       timestamp: new Date()
     }
