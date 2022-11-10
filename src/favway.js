5ac9219b5540384122d99f01e16244dba975c3b3
Bread <72562619+NotRealArif@users.noreply.github.com>
2022-11-10 14:52:11 +0000

    Fixef

--- src/favway.js
+++ src/favway.js
@@ -15,7 +15,7 @@ import { loadCommands } from "./handlers/commands.js";
 console.log(colour.blueBright("[System] Index loading..."));
 
 const client = new Client(process.env.TOKEN, {
-  restMode: true,
+  restMode: false,
   intents: [
     "guilds",
     "guildMessages",
