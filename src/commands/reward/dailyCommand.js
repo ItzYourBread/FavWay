be43e28ce07f46dc138ea02d27d6a1c62bce717f
Bread <72562619+NotRealArif@users.noreply.github.com>
2022-11-22 20:33:03 +0000

    Daily command completed

--- src/commands/reward/dailyCommand.js
+++ src/commands/reward/dailyCommand.js
@@ -1,8 +1,6 @@
 import { User } from "../../database/profile.js";
 import { ms } from "printly.js";
 import config from "../../config.json" assert { type: "json" };
-import moment from "moment";
-import "moment-duration-format";
 
 export default {
   data: {
@@ -14,26 +12,22 @@ export default {
     const user = interaction.member;
     const userData = await User.findOne({ id: user.id }) || new User({ id: user.id });
 
-    const duration = moment
-        .duration(userData.cooldowns.daily - Date.now())
-        .format("h[h] m[m], s[s]");
-
-    /* if (user && userData.cooldowns.daily > Date.now()) {
+    if (user && userData.cooldowns.daily > Date.now()) {
       return interaction.createMessage({
         embeds: [{
           title: "Already claimed!",
           color: Number(config.colours.error),
-          description: `You have already claimed today daily reward!\nWait ${duration} for next daily reward.`,
+          description: `You have already claimed today daily reward!\nYour next daily <t:${Math.floor(userData.cooldowns.daily / 1000) + 3600}:R>`,
           timestamp: new Date()
         }],
         flags: 64
       });
-    } */
+    } 
 
-    let streakResetMessage = "";
+    let streakReset = false;
     if (Date.now() - userData.cooldowns.daily > 172800000) { // 172800000 = 2 days
       userData.streaks.daily = 1;
-      streakResetMessage += "Seens you didnt claimed your daily to daily, i reset your streak to 1 back!";
+      streakReset = true;
     } else {
       userData.streaks.daily += 1;
     }
@@ -47,20 +41,20 @@ export default {
     if (streak > 1) {
       amount += amount + streakBonus;
     }
-    if (userData.prestige > 1) {
+    if (userData.prestige >= 1) {
       prestigeBonus += Math.floor(Math.random() * 500) + 100;
     }
 
     userData.coins += amount + prestigeBonus;
     userData.foods.apples += 2;
     userData.items.dailyCrate += 1;
-    userData.cooldowns.daily = dailyReset.setUTCHours(23,59,59,999);;
+    userData.cooldowns.daily = dailyReset.setUTCHours(23,59,59,999);
     userData.save();
     
     let reward = {
       title: "Daily claimed!",
       color: 0xcec6ff,
-      description: `+ ${amount.toLocaleString()} **${config.emojis.coin}Coin**\n+ 2 **${config.emojis.apple}Apple**`,
+      description: `+${amount.toLocaleString()} **${config.emojis.coin}Coin**\n+2 **${config.emojis.apple}Apple**`,
       fields: [
         {
           name: "Prestige bonus",
@@ -81,6 +75,15 @@ export default {
       timestamp: new Date()
     }
 
-    await interaction.createMessage({ content: `\ ${streakResetMessage}  \ `, embeds: [reward] });
+    await interaction.createMessage({ embeds: [reward] });
+
+    if (streakReset) {
+      await interaction.createFollowup({
+        embeds: [{
+          color: Number(config.colours.embed),
+          description: `Your daily streak has been reset, seem you didn't claimed daily reward from <t:${Math.floor(userData.cooldowns.daily / 1000) + 3600}:R>`,
+        }]
+      });
+    }
   }
 }
